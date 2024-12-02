bac220a0b4e5fda3999d02f2a8c6adb20b3b78e7
shubham rawat <rawatsubham9837@gmail.com>
2024-12-02 11:31:44 +0000

    datatype

---
+++ 01-java/conversion.js
@@ -0,0 +1,15 @@
+// let score = "cf45"
+// let score = 4
+// let score = undefined
+// let score = null
+// let score = true
+// let score = 0
+
+console.log( typeof score);
+console.log( typeof(score));
+
+let number = Number(score)
+console.log( number)
+
+let b = Boolean( score)
+console.log(b)
---
+++ 01-java/datatype.js
@@ -0,0 +1,16 @@
+"use strict"; 
+// treat all js code as newser version
+console.log(3+3);
+
+
+// data type 
+// number
+// bigint
+// string
+// Boolean
+// null
+// undefined
+// Symbol
+
+console.log( typeof undefined) //undefined
+console.log( typeof null)//object
