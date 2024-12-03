ddf65bb7e7a6df686d8f95185d4d49e47e37d888
shubham rawat <rawatsubham9837@gmail.com>
2024-12-03 17:25:33 +0000

    js

---
+++ 01-java/string.js
@@ -0,0 +1,12 @@
+let a =5
+let b = 3
+
+let c = a+b
+console.log(c);
+
+console.log( "1"+3);
+console.log(3+"4");
+console.log("3"+2+3);
+console.log(+true)
+
+
--- 01-java/var.js
+++ 01-java/var.js
@@ -3,6 +3,6 @@ let accountEmail="shubham"
 var accountpassword ="234"
 accountcity="uttrakand"
 
-accountId =2 
+// accountId =2 
 
 console.log(accountId)
