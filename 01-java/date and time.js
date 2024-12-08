60fa5d45c457ada0dab1b8b199598cf42d80057b
shubham rawat <rawatsubham9837@gmail.com>
2024-12-08 08:16:55 +0000

    update

---
+++ 01-java/date and time.js
@@ -0,0 +1,13 @@
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate)
+
+let mycreatedate = new Date(2023,0,23)
+// let mycreatedate = new Date ( " 01-14-2023")
+console.log( mycreatedate.toLocaleString())
+ 
+// let mycreatedate = Date.now()
+
+console.log(mycreatedate)
