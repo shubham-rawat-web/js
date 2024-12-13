7a50d94f9c5446fad66ade8583e34ebfb342dab7
shubham rawat <rawatsubham9837@gmail.com>
2024-12-13 17:39:15 +0000

    function

---
+++ 01-java/function.js
@@ -0,0 +1,35 @@
+function myname () 
+{
+  console.log("hii")
+}
+
+myname()
+
+
+function addtwonum (num1, num2) {
+
+  return num1 + num2
+}
+let a = addtwonum(3,4)
+
+console.log("result :",a)
+
+
+function add (num1, num2) {
+
+ let ans = num1 + num2
+ return ans
+}
+let b = add(3,4)
+
+console.log("result :",b)
+
+function loginuser (username) {
+    
+    return ` ${username} just loggged in`
+}
+
+ console.log (loginuser("hitesh") )  
+
+
+
