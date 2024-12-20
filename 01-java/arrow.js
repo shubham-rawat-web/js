392a0609682d7b6d5102ada0312c2f6c90b21a9c
shubham rawat <rawatsubham9837@gmail.com>
2024-12-20 06:23:20 +0000

    arrow

---
+++ 01-java/arrow.js
@@ -0,0 +1,23 @@
+const user = {
+    username : "shubham",
+    price : 999,
+    welcomemessage:function(){
+        console.log(`${this.username},welcome to website`)
+    }
+}
+
+// const addtwo= (num1,num2) =>
+// {
+//     return num1 + num2
+// }
+// console.log( addtwo(3,4))
+
+
+// const addtwo= (num1,num2) => num1 + num2
+// const addtwo= (num1,num2) => (num1 + num2)
+// const addtwo= (num1,num2) => ({ username:"hitesh"})
+
+
+
+console.log( addtwo(3,4))
+
